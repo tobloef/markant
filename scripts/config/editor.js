3f9a36b018048252773ccb995daa70ab5a08b774
Tobias Løfgren <tobloef@gmail.com>
2016-10-26 21:48:24 +0200

    Removed default text

--- scripts/config/editor.js
+++ scripts/config/editor.js
@@ -6,25 +6,7 @@
 			lineWrapping: true,
 			lineNumbers: false,
 			autofocus: true,
-			value: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Hoc Hieronymus summum bonum esse dixit. Illa videamus, quae a te de amicitia dicta sunt. Tum Quintus: Est plane, Piso, ut dicis, inquit.
-
-Quos quidem tibi studiose et diligenter tractandos magnopere censeo. Haeret in salebra. Ego quoque, inquit, didicerim libentius si quid attuleris, quam te reprehenderim. Videmusne ut pueri ne verberibus quidem a contemplandis rebus perquirendisque deterreantur?
-
-Duo Reges: constructio interrete. Nam his libris eum malo quam reliquo ornatu villae delectari. Ita relinquet duas, de quibus etiam atque etiam consideret.
-
-Itaque in rebus minime obscuris non multus est apud eos disserendi labor. Quod cum ille dixisset et satis disputatum videretur, in oppidum ad Pomponium perreximus omnes. Terram, mihi crede, ea lanx et maria deprimet. Quo plebiscito decreta a senatu est consuli quaestio Cn. Ita multa dicunt, quae vix intellegam.
-
-Sed quot homines, tot sententiae; Mihi enim satis est, ipsis non satis. Omnia contraria, quos etiam insanos esse vultis. Quod autem in homine praestantissimum atque optimum est, id deseruit. Tum ille: Ain tandem? Disserendi artem nullam habuit. Hosne igitur laudas et hanc eorum, inquam, sententiam sequi nos censes oportere?
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Hoc Hieronymus summum bonum esse dixit. Illa videamus, quae a te de amicitia dicta sunt. Tum Quintus: Est plane, Piso, ut dicis, inquit.
-
-Quos quidem tibi studiose et diligenter tractandos magnopere censeo. Haeret in salebra. Ego quoque, inquit, didicerim libentius si quid attuleris, quam te reprehenderim. Videmusne ut pueri ne verberibus quidem a contemplandis rebus perquirendisque deterreantur?
-
-Duo Reges: constructio interrete. Nam his libris eum malo quam reliquo ornatu villae delectari. Ita relinquet duas, de quibus etiam atque etiam consideret.
-
-Itaque in rebus minime obscuris non multus est apud eos disserendi labor. Quod cum ille dixisset et satis disputatum videretur, in oppidum ad Pomponium perreximus omnes. Terram, mihi crede, ea lanx et maria deprimet. Quo plebiscito decreta a senatu est consuli quaestio Cn. Ita multa dicunt, quae vix intellegam.
-
-Sed quot homines, tot sententiae; Mihi enim satis est, ipsis non satis. Omnia contraria, quos etiam insanos esse vultis. Quod autem in homine praestantissimum atque optimum est, id deseruit. Tum ille: Ain tandem? Disserendi artem nullam habuit. Hosne igitur laudas et hanc eorum, inquam, sententiam sequi nos censes oportere?`,
+			value: ``,
 		},
 		// Id of the drag bar element, used for resizing the editor and viewer panes.
 		dragbarId: "dragbar",
